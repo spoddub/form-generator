fb88ff49e5abe32311bf6ffc6a078b60c48fbb32
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-09 01:56:34 +0300

    add tag generator

---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module HexletCode
+  module Tag
+    SINGLE_TAGS = %w[br img input].freeze
+
+    def self.build(name, attrs = {})
+      name = name.to_s
+      attrs_str = build_attrs(attrs)
+
+      if SINGLE_TAGS.include?(name)
+        "<#{name}#{attrs_str}>"
+      else
+        content = block_given? ? yield : ""
+        "<#{name}#{attrs_str}>#{content}</#{name}>"
+      end
+    end
+
+    def self.build_attrs(attrs)
+      return "" if attrs.nil? || attrs.empty?
+
+      attrs
+        .sort_by { |k, _| k.to_s }
+        .map { |k, v| " #{k}=\"#{v}\"" }
+        .join
+    end
+  end
+end
