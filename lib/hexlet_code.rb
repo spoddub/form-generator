e2d91532f35d2339de46ae880efd86da0abcf7ac
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-09 18:51:44 +0300

    fix user creation conflict

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,13 +5,16 @@ require_relative 'hexlet_code/version'
 module HexletCode
   autoload :Tag, 'hexlet_code/tag'
   autoload :FormBuilder, 'hexlet_code/form_builder'
-  def self.form_for(_entity, **options, &block)
+
+  def self.form_for(entity, **options, &block)
     action = options.key?(:url) ? options.delete(:url) : '#'
     method = 'post'
     form_attrs = { action:, method: }.merge(options)
+
     builder = FormBuilder.new(entity)
     block&.call(builder)
     body = builder.render
+
     Tag.build('form', form_attrs) { body }
   end
 end
