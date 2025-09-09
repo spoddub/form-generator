7cc091bfd426ac23e81bac6ed74f86f7e50f9b6a
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-09 03:09:51 +0300

    fix linting

--- test/form_for_test.rb
+++ test/form_for_test.rb
@@ -11,13 +11,19 @@ class FormForTest < Minitest::Test
   end
 
   def test_empty_form_with_defaults
-    html = HexletCode.form_for(@user) { |f| }
+    html = HexletCode.form_for(@user) { |_f| nil }
     expected = '<form action="#" method="post"></form>'
     assert_equal expected, html
   end
 
+  def test_empty_form_with_class
+    html = HexletCode.form_for(@user, class: 'hexlet-form') { |_f| nil }
+    expected = '<form action="#" method="post" class="hexlet-form"></form>'
+    assert_equal expected, html
+  end
+
   def test_empty_form_with_url_and_class
-    html = HexletCode.form_for(@user, url: '/profile', class: 'hexlet-form') { |f| }
+    html = HexletCode.form_for(@user, url: '/profile', class: 'hexlet-form') { |_f| nil }
     expected = '<form action="/profile" method="post" class="hexlet-form"></form>'
     assert_equal expected, html
   end
