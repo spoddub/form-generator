86a56ce71c1c60b5fa7117c0988c063053936579
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-09 19:55:19 +0300

    fix rubocop

--- test/input_test.rb
+++ test/input_test.rb
@@ -5,57 +5,58 @@ require 'hexlet_code'
 
 InputUser = Struct.new(:name, :job, :gender, keyword_init: true)
 
+INPUT_EXPECTED_DEFAULTS =
+  '<form action="#" method="post">' \
+  '<label for="name">Name</label>' \
+  '<input name="name" type="text" value="rob">' \
+  '<textarea name="job" cols="20" rows="40">hexlet</textarea>' \
+  '</form>'
+
+INPUT_EXPECTED_EXTRA_ATTRS =
+  '<form action="#" method="post">' \
+  '<label for="name">Name</label>' \
+  '<input name="name" type="text" value="rob" class="user-input">' \
+  '<label for="job">Job</label>' \
+  '<input name="job" type="text" value="hexlet">' \
+  '</form>'
+
+INPUT_EXPECTED_TEXTAREA_OVERRIDE =
+  '<form action="#" method="post">' \
+  '<textarea name="job" cols="50" rows="50">hexlet</textarea>' \
+  '</form>'
+
 class InputTest < Minitest::Test
   def setup
     @user = InputUser.new(name: 'rob', job: 'hexlet', gender: 'm')
   end
 
+  # Хелпер, чтобы короче было в самих тестах
+  def build_form(options = {}, &)
+    HexletCode.form_for(@user, **options, &)
+  end
+
   def test_text_and_textarea_defaults
-    html = HexletCode.form_for(@user) do |f|
-      f.input :name
-      f.input :job, as: :text
+    html = build_form do |f|
+      f.input(:name)
+      f.input(:job, as: :text)
     end
-
-    expected = '<form action="#" method="post">' \
-               '<label for="name">Name</label>' \
-               '<input name="name" type="text" value="rob">' \
-               '<textarea name="job" cols="20" rows="40">hexlet</textarea>' \
-               '</form>'
-
-    assert_equal expected, html
+    assert_equal INPUT_EXPECTED_DEFAULTS, html
   end
 
   def test_input_with_extra_attrs
-    html = HexletCode.form_for(@user, url: '#') do |f|
-      f.input :name, class: 'user-input'
-      f.input :job
+    html = build_form(url: '#') do |f|
+      f.input(:name, class: 'user-input')
+      f.input(:job)
     end
-
-    expected = '<form action="#" method="post">' \
-               '<label for="name">Name</label>' \
-               '<input name="name" type="text" value="rob" class="user-input">' \
-               '<label for="job">Job</label>' \
-               '<input name="job" type="text" value="hexlet">' \
-               '</form>'
-
-    assert_equal expected, html
+    assert_equal INPUT_EXPECTED_EXTRA_ATTRS, html
   end
 
   def test_textarea_override_rows_cols
-    html = HexletCode.form_for(@user) do |f|
-      f.input :job, as: :text, rows: 50, cols: 50
-    end
-
-    expected = '<form action="#" method="post">' \
-               '<textarea name="job" cols="50" rows="50">hexlet</textarea>' \
-               '</form>'
-
-    assert_equal expected, html
+    html = build_form { |f| f.input(:job, as: :text, rows: 50, cols: 50) }
+    assert_equal INPUT_EXPECTED_TEXTAREA_OVERRIDE, html
   end
 
   def test_missing_field_raises
-    assert_raises(NoMethodError) do
-      HexletCode.form_for(@user) { |f| f.input :age }
-    end
+    assert_raises(NoMethodError) { build_form { |f| f.input :age } }
   end
 end
--- test/submit_test.rb
+++ test/submit_test.rb
@@ -5,44 +5,48 @@ require 'hexlet_code'
 
 SubmitUser = Struct.new(:name, :job, keyword_init: true)
 
+SUBMIT_EXPECTED_DEFAULT =
+  '<form action="#" method="post">' \
+  '<label for="name">Name</label>' \
+  '<input name="name" type="text" value="">' \
+  '<label for="job">Job</label>' \
+  '<input name="job" type="text" value="hexlet">' \
+  '<input type="submit" value="Save">' \
+  '</form>'
+
+SUBMIT_EXPECTED_CUSTOM =
+  '<form action="#" method="post">' \
+  '<label for="name">Name</label>' \
+  '<input name="name" type="text" value="">' \
+  '<label for="job">Job</label>' \
+  '<input name="job" type="text" value="hexlet">' \
+  '<input type="submit" value="Wow">' \
+  '</form>'
+
 class SubmitTest < Minitest::Test
   def setup
-    @user = SubmitUser.new(job: 'hexlet') # name => nil
+    @user = SubmitUser.new(job: 'hexlet')
+  end
+
+  def build_form(options = {}, &)
+    HexletCode.form_for(@user, **options, &)
   end
 
   def test_submit_with_default_text_and_labels
-    html = HexletCode.form_for(@user) do |f|
-      f.input :name
-      f.input :job
+    html = build_form do |f|
+      f.input(:name)
+      f.input(:job)
       f.submit
     end
-
-    expected = '<form action="#" method="post">' \
-               '<label for="name">Name</label>' \
-               '<input name="name" type="text" value="">' \
-               '<label for="job">Job</label>' \
-               '<input name="job" type="text" value="hexlet">' \
-               '<input type="submit" value="Save">' \
-               '</form>'
-
-    assert_equal expected, html
+    assert_equal SUBMIT_EXPECTED_DEFAULT, html
   end
 
   def test_submit_with_custom_text
-    html = HexletCode.form_for(@user, url: '#') do |f|
-      f.input :name
-      f.input :job
-      f.submit 'Wow'
+    html = build_form(url: '#') do |f|
+      f.input(:name)
+      f.input(:job)
+      f.submit('Wow')
     end
-
-    expected = '<form action="#" method="post">' \
-               '<label for="name">Name</label>' \
-               '<input name="name" type="text" value="">' \
-               '<label for="job">Job</label>' \
-               '<input name="job" type="text" value="hexlet">' \
-               '<input type="submit" value="Wow">' \
-               '</form>'
-
-    assert_equal expected, html
+    assert_equal SUBMIT_EXPECTED_CUSTOM, html
   end
 end
