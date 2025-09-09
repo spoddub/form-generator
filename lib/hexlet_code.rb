a4a8207951bded8929d52e78957ff0fc737201b9
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-09 03:04:24 +0300

    add form builder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,4 +4,11 @@ require_relative 'hexlet_code/version'
 
 module HexletCode
   autoload :Tag, 'hexlet_code/tag'
+  def self.form_for(entity, **options, &block)
+    action = options.key?(:url) ? options.delete(:url) : '#'
+    method = 'post'
+    form_attrs = { action:, method: }.merge(options)
+    block.call(nil) if block
+    Tag.build('form', form_attrs) { '' }
+  end
 end
