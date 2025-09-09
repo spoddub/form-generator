31e36430191fd91f8f760016baf7833b66aa5e0d
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-09 18:39:56 +0300

    add autoload for form builder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,11 +4,13 @@ require_relative 'hexlet_code/version'
 
 module HexletCode
   autoload :Tag, 'hexlet_code/tag'
+  autoload :FormBuilder, 'hexlet_code/form_builder'
   def self.form_for(_entity, **options, &block)
     action = options.key?(:url) ? options.delete(:url) : '#'
     method = 'post'
     form_attrs = { action:, method: }.merge(options)
-    block&.call(nil)
-    Tag.build('form', form_attrs) { '' }
+    builder = FormBuilder.new(entity)
+    block&.call(builder)
+    Tag.build('form', form_attrs) { body }
   end
 end
